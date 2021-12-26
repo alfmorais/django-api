bfe47f0cc188c9f19ec3d9721399dafdb3464362
alfmorais <alfredneto@hotmail.com>
2021-12-26 10:49:02 -0300

    criando o app core e pasta api

---
+++ src/core/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import PontoTuristico
+
+
+@admin.register(PontoTuristico)
+class PontoTuristicoAdmin(admin.ModelAdmin):
+    list_display = (
+        "nome",
+        "descricao",
+        "aprovado",
+    )
