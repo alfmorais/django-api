1c99baad710f510676ba2e591acacd7f8cb50013
alfmorais <alfredneto@hotmail.com>
2021-12-27 21:12:54 -0300

    implementando a viewset e o serializer de pontos turisticos

--- src/core/models.py
+++ src/core/models.py
@@ -11,7 +11,7 @@ class PontoTuristico(models.Model):
     atracao = models.ManyToManyField(Atracao)
     comentario = models.ManyToManyField(Comentario)
     avaliacoes = models.ManyToManyField(Avaliacoes)
-    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
+    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f"{self.nome}"
--- src/turismo/urls.py
+++ src/turismo/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+from rest_framework import routers
+from core.api.viewsets import PontoTuristicoViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'pontoturistico', PontoTuristicoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
