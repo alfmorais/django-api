3801836d749f692b62d8ac71c601fba9feff3dbc
alfmorais <alfredneto@hotmail.com>
2021-12-31 16:17:28 -0300

    implementando a funçao list para app core

--- src/core/api/viewsets.py
+++ src/core/api/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
@@ -9,3 +10,13 @@ class PontoTuristicoViewSet(ModelViewSet):
     def get_queryset(self):
         queryset = PontoTuristico.objects.filter(aprovado=True)
         return queryset
+    
+    """
+    def list(self, request, *args, **kwargs):
+        dictionary = {
+            "nome": "Alfredo de Morais Neto",
+            "idade": 29,
+            "estado civil": "casado",
+        }
+        return Response(dictionary)
+    """
